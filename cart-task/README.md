a817cca2f83d5a3969db42e26cf7d776a55e1662
renukakathirvel <renukakathirvel18@gmail.com>
2024-10-18 15:15:27 +0530

    first commit

---
+++ cart-task/README.md
@@ -0,0 +1,34 @@
+React Shopping Cart
+A responsive shopping cart application built with React, inspired by the Start Bootstrap Shop Homepage design. Users can browse products and manage their shopping cart by adding and removing items.
+
+Preview
+
+
+Features
+Product List: Display a list of available products with their name and description.
+Add to Cart: Allows users to add items to the cart by clicking the "Add to Cart" button.
+Cart Management: Updates the cart quantity when items are added or removed.
+Toggle Button: Changes the "Add to Cart" button to "Remove from Cart" once an item is added, and vice versa.
+Technologies Used
+React: For building the user interface.
+CSS: For styling the application.
+Bootstrap: For layout and design elements.
+How to Run the Application
+Clone the repository:
+git clone 
+
+Navigate to the project directory:
+cd Shopping-cart
+Install dependencies:
+npm install
+Start the development server:
+npm start
+Development Process
+The development involved several stages:
+
+Initial Setup: Setting up the React environment and project structure.
+Product Listing: Creating components for displaying product information.
+Cart Management: Implementing state management for adding and removing items from the cart.
+Styling: Applying CSS and Bootstrap for layout and design consistency.
+Conclusion
+This project demonstrates basic shopping cart functionality using React, providing a foundation for further enhancements and integration with backend services for a full-fledged e-commerce application.
---
+++ cart-task/src/pages/products.jsx
@@ -0,0 +1,76 @@
+import imageProduct1 from "../assests/images/1.png"
+import imageProduct2 from "../assests/images/2.png"
+import imageProduct3 from "../assests/images/3.png"
+import imageProduct4 from "../assests/images/4.png"
+import imageProduct5 from "../assests/images/5.png"
+import imageProduct6 from "../assests/images/6.png"
+import imageProduct7 from "../assests/images/7.png"
+import imageProduct8 from "../assests/images/8.png"
+
+
+export const products = [
+    {
+        id: 1,
+        name: "Unisex Waterproof Sport Digital Watch",
+        price: 299,
+        image: imageProduct1,
+        description: "Unisex Waterproof Sport Digital Watch For Men - Women & Children Digital Watch - For Boys & Girls Unique Designed Black Round Big Dial Digital Watch For Kids - Boys And Girls",
+        slug: "funblast-unicorn-soft-toy"   
+    },
+    {
+        id: 2,
+        name: "Mens Watch Chronograph Stainless Steel",
+        price: 550,
+        image: imageProduct2,
+        description: "G This versatile watch blend goes with every occasion be it formal or casual. Wear this watch on the go, from business, leisure to indoor and outdoor activities or for daily use.",
+        slug: "mens-watches-chronograph-stainless-steel-waterproof"   
+    },
+    {
+        id: 3,
+        name: "Paradise Dancing Peacock Showpiece",
+        price: 700,
+        image: imageProduct3,
+        description: "HANDICRAFTS PARADISE Brown & Green Figurine Paradise Dancing Peacock Showpiece",
+        slug: "brown-Green-figurine-paradise-dancing-peacock-showpiece"   
+    },
+    {
+        id: 4,
+        name:"Sitting Jerry Soft Toy",
+        price: 705,
+        image: imageProduct4,
+        description: "ur Jerry Soft Toy is thoughtfully designed to capture the timeless charm of this iconic cartoon character",
+        slug: "sitting-jerry-soft-toy"   
+    },
+    {
+        id: 5,
+        name: "Flower Tree",
+        price: 950,
+        image: imageProduct5,
+        description: "flower Tree metal tree decor Indian metal show piece",
+        slug: "flower-tree"   
+    },
+    {
+        id: 6,
+        name: "DIKANG Sitting Unicorn Soft Toys for Kids",
+        price: 349,
+        image: imageProduct6,
+        description: "Looking for a soft toy which is also washable when it becomes dirty ? Our Soft Toys are made up of washable materials. You can wash them in washing machine or by hand to keep your toy clean. And it is a very important for safety of your kids",
+        slug: "dikang-sitting-unicorn-soft-toys-forkids-toy"    
+    },
+    {
+        id: 7,
+        name: "Best Tom Ford Perfume",
+        price: 999,
+        image: imageProduct7,
+        description: "Tom Ford’s “Soleil” collection features three different takes on what a sunny adventure would smell like, and Soleil Brulant is the most interesting. Where Soleil Neige, Blanc, and De Fu are playful, lighter interpretations, Soleil Brulant is what happens when orange blossom, black honey, and pink pepper lay out in the sun for long.",
+        slug: "best-tom-ford-perfume"   
+    },
+    {
+        id: 8,
+        name: "Eau De Parfum Spray For Women",
+        price: 749,
+        image: imageProduct8,
+        description: "Embodies sophistication with a classic and refined fragrance profile.Features fresh mandarin and bergamot for an uplifting, energetic start.Blends delicate rose, jasmine, and peony for a romantic and graceful aroma.",
+        slug: "dior-christian-miss,3.4-eau-de-parfum-spray-for-women"
+    }
+    ]
